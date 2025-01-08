e0865b8cad655ab35691043589a09313c35a33c5
Jan Nielsen <jan.sture.nielsen@gmail.com>
2025-01-08 09:47:12 -0700

    Adding scrollTo functionality.

--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, computed, effect, ElementRef, viewChild} from '@angular/core';
 
 @Component({
   selector: 'my-window',
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './window.component.scss'
 })
 export class WindowComponent {
+  myWindow = viewChild.required<ElementRef>('myWindow');
+
+  #scrollTo = computed(() => {
+    const nativeElement = this.myWindow().nativeElement;
+    const scrollHeight: number = nativeElement.scrollHeight;
+    const offsetHeight: number = nativeElement.offsetHeight;
+
+    const scrollTo = 4 + (scrollHeight - offsetHeight) / 2;
+
+    console.log('jsn: scroll-to:', scrollTo, '(', scrollHeight, ', ', offsetHeight, ')', nativeElement);
+
+    return scrollTo;
+  });
+
+  constructor() {
+    effect(() => this.myWindow().nativeElement.scrollTo(0, this.#scrollTo()));
+  }
 
 }
