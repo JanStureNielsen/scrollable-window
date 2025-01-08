23097abac2319e4e45396efb87cb6d5b5fa8c0a9
Jan Nielsen <jan.sture.nielsen@gmail.com>
2025-01-08 10:54:24 -0700

    Generate some content.

--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -7,7 +7,38 @@ import {Component, computed, effect, ElementRef, viewChild} from '@angular/core'
   styleUrl: './window.component.scss'
 })
 export class WindowComponent {
+  #contentSize = 100;
+  #contentGap = 6;
+
   myWindow = viewChild.required<ElementRef>('myWindow');
+  redContent = computed(() => {
+    const myContent = [];
+    for (let i = this.#contentSize; i >= this.#contentSize/2 + this.#contentGap/2; i--) {
+      myContent.push({id: i});
+    }
+    console.log('jsn: red: ', myContent);
+    return myContent;
+  });
+
+  gapContent = computed(() => {
+    const reds = this.redContent();
+    const blues = this.blueContent();
+
+    // if ordered...
+    const gap = reds[reds.length-1].id - blues[0].id;
+
+    console.log('jsn: gap: ', gap);
+
+    return gap;
+  });
+  blueContent = computed(() => {
+    const myContent = [];
+    for (let i = this.#contentSize/2 - this.#contentGap/2; i >= 0; i--) {
+      myContent.push({id: i});
+    }
+    console.log('jsn: blue: ', myContent);
+    return myContent;
+  });
 
   #scrollTo = computed(() => {
     const nativeElement = this.myWindow().nativeElement;
