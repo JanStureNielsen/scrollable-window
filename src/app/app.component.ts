cf970b03d6208feeca16e9c0eeb853a32faec8f3
Jan Nielsen <jan.sture.nielsen@gmail.com>
2025-01-08 09:34:27 -0700

    Add a window component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {WindowComponent} from './window/window.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WindowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
