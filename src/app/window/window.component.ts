eb6168b7b819bf2963dc1b2761e7b0860dd12c76
Jan Nielsen <jan.sture.nielsen@gmail.com>
2025-01-08 17:54:35 -0700

    Simplify sample.

--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -18,31 +18,15 @@ export class WindowComponent {
   }
 
   onCenter() {
-    return this.#doScrollTo(this.#scrollTo());
-  }
-
-  #doOnCenter(onCenter: boolean) {
-    console.log('jsn: do-on-center', onCenter);
-    //ignore argument
-    this.onCenter();
-  }
-
-  #scrollTo() {
     const nativeElement = this.myWindow().nativeElement;
     const scrollHeight: number = nativeElement.scrollHeight;
     const offsetHeight: number = nativeElement.offsetHeight;
 
     const scrollTo = 4 + (scrollHeight - offsetHeight) / 2;
 
-    console.log('jsn: scroll-to:', scrollTo, '(', scrollHeight, ', ', offsetHeight, ')', nativeElement);
+    //console.log('jsn: scroll-to:', scrollTo, '(', scrollHeight, ', ', offsetHeight, ')', nativeElement);
 
-    return scrollTo;
-  }
-
-  #doScrollTo(scrollTo: number) {
     this.myWindow().nativeElement.scrollTo(0, scrollTo);
-
-    return scrollTo;
   }
 
   #buildContent(size: number) {
